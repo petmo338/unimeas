1a7027e735d0f27a5a4a89e95d8c698d10e41883
Peter Moller <petmo338@gmail.com>
2014-11-27 23:17:09 +0100

    Basic database interfaceing now works

--- program/database/measurement_session.py
+++ program/database/measurement_session.py
@@ -1,6 +1,5 @@
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Table
-from sqlalchemy.sql import functions, text
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Table, DDL, Float
+from sqlalchemy.sql import functions
 from sqlalchemy.ext.compiler import compiles
 from base import Base
 import logging
@@ -33,13 +32,13 @@ class MeasurementSession(Base):
     __tablename__ = 'measurement_sessions'
     
     id = Column(Integer, primary_key=True)
-    name = Column(String)
+    name = Column(String(256))
     instrument_id = Column(Integer, ForeignKey('instruments.id'))
     user = Column(Integer, ForeignKey('users.id'))
-    data_table = Column(String, unique = True, default='NOT SET')
-    description = Column(String)
-    sensor_id = Column(String)
-    gasmixer_system = Column(String)                    
+    data_table = Column(String(256), unique = True, default='NOT SET')
+    description = Column(String(256))
+    sensor_id = Column(String(256))
+    gasmixer_system = Column(String(256))                    
     measurement_class_id = Column(Integer, ForeignKey('measurement_classes.id'))
     created_at = Column(DateTime(timezone=True), nullable=False, default=utcnow())
     
@@ -47,16 +46,13 @@ class MeasurementSession(Base):
         if hasattr(self, 'engine'):
             table = 'data_table_%d' % self.id
             if self.engine.has_table(table):
-                    logger.error('Data table already exist. Strange!')
+                    logger.error('Data table already exist. Strange! Dropping table.')
                     query = 'DROP TABLE ' + table 
                     self.engine.execute(query)
             if self.measurement_class.measurement_type.find('INTERVAL') is 0:
-                tmp_table = Table(table, Base.metadata, Column('id', Integer, primary_key=True))
-
-                #query = text('CREATE TABLE :tbl (id serial)')
+                self.measurement_data = Table(table, Base.metadata, Column('id', Integer, primary_key=True))
                 try:
-                    tmp_table.create(self.engine) 
-                    #self.engine.execute(query, tbl = table)
+                    self.measurement_data.create(self.engine) 
                 except Exception as e:
                     logger.error(e)
                     return
@@ -66,24 +62,30 @@ class MeasurementSession(Base):
                         query = 'INSERT INTO ' + table + ' VALUES(default)'
                         for row in xrange(self.measurement_class.number_of_samples):
                             conn.execute(query)
-
+                    
             elif self.measurement_class.measurement_type.find('TIME') is 0:
-                table = 'data_table_%d' % self.id
-                query = 'CREATE TABLE public.' + table + '(id serial, created_at time with time zone NOT NULL DEFAULT \
-                    (CURRENT_TIMESTAMP AT TIME ZONE \'utc\')::TIMESTAMP WITH TIME ZONE) WITH (OIDS = FALSE)'
+                self.measurement_data = Table(table, Base.metadata, Column('id', Integer, primary_key=True), \
+                    Column('created_at', DateTime(timezone=True), nullable=False, default=utcnow()))
+                for column in  self.measurement_class.data_columns.split(','):
+                    self.measurement_data.append_column(Column(column, Float))
+                try:
+                    self.measurement_data.create(self.engine) 
+                except Exception as e:
+                    logger.error(e)
+                    return
     
     def new_run(self, columns):
         if hasattr(self, 'engine'):
             if self.measurement_class.measurement_type.find('INTERVAL') is 0:
                 for column in columns:
-                    query = text('ALTER TABLE public.:tbl ADD COLUMN :clm double precision')
+                    stmt = DDL('ALTER TABLE %(table)s ADD COLUMN %(clm)s double precision', context = {'table':self.data_table,'clm':column})
                     try:
-                        self.engine.execute(query, tbl = self.data_table, clm = column)
+                        stmt.execute(self.engine)
                     except Exception as e:
                         logger.error(e)
                         return False
+                    self.measurement_data.append_column(Column(column, Float))
 
-                self.measurement_data = Table(self.data_table, Base.metadata, autoload = True, autoload_with = self.engine)
                 self.measurement_data.active_columns = columns
                 return True
 
@@ -91,9 +93,13 @@ class MeasurementSession(Base):
         if hasattr(self, 'engine'):
             if self.measurement_class.measurement_type.find('INTERVAL') is 0:
                 with self.engine.begin() as conn:
+                    d = dict()
                     for column in self.measurement_data.active_columns:
-                        stmt = self.measurement_data.update().where(self.measurement_data.id == data['sample_nr']).values(column = data[column])
-                        conn.execute(query)
-
+                        d[column] = data[column]
+                    stmt = self.measurement_data.update().where(self.measurement_data.c.id == data['sample_nr']).values(d)
+                    conn.execute(stmt)
+            elif self.measurement_class.measurement_type.find('TIME') is 0:
+                stmt = self.measurement_data.insert().values(data)
+                self.engine.execute(stmt)
                     
         

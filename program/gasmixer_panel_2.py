52112d618cb83225952d51eb44a038c2c81c3f82
Peter Moller <petmo338@gmail.com>
2016-12-06 21:54:08 +0100

    moved gasmixer polling to separate thread

--- program/gasmixer_panel_2.py
+++ program/gasmixer_panel_2.py
@@ -2,6 +2,11 @@ import logging
 from traits.api import HasTraits, Int, Bool, Str, Event, Instance, Dict, Tuple
 from traitsui.api import Item, View, Handler, ButtonEditor,  HGroup, spring, Label
 from pyface.timer.api import Timer
+import threading
+import Queue
+import json
+
+from time import sleep
 try:
     import zmq
 except ImportError as e:
@@ -32,14 +37,54 @@ class State:
                STOPPING: 'Stopping'}
 
 
+
+class GasMixerSubscriber(threading.Thread):
+    def __init__(self, sub_queue, control_queue):
+        super(GasMixerSubscriber, self).__init__()
+        self.exit_flag = False
+        try:
+            self.context = zmq.Context().instance()
+            self.subscriber = self.context.socket(zmq.SUB)
+            self.subscriber.connect('tcp://localhost:5561')
+            self.subscriber.setsockopt(zmq.SUBSCRIBE, "")
+            self.control = self.context.socket(zmq.REQ)
+            self.control.connect('tcp://localhost:5562')
+            self.poller = zmq.Poller()
+            self.poller.register(self.subscriber, zmq.POLLIN)
+            self.sub_queue = sub_queue
+            self.control_queue = control_queue
+        except:
+            return None
+
+
+
+    def run(self):
+        while not self.exit_flag:
+            socks = dict(self.poller.poll(POLL_TIMEOUT))
+            logger.warning(socks)
+            if self.subscriber in socks and socks[self.subscriber] == zmq.POLLIN:
+                msg = self.subscriber.recv()
+                self.sub_queue.put_nowait(msg)
+            # elif self.control in socks and socks[self.control] == zmq.POLLIN:
+
+            if not self.control_queue.empty():
+                self.control.send(self.control_queue.get())
+                self.control_queue.task_done()
+                msg = self.control.recv()
+                self.sub_queue.put_nowait(msg)
+
+            sleep(0.5)
+        self.sub_queue.put_nowait('KILLINGMYSELF')
+
 class GasMixerPanelHandler(Handler):
 
     def closed(self, info, is_ok):
         """ Handles a dialog-based user interface being closed by the user.
         Overridden here to stop the timer once the window is destroyed.
         """
-        if info.object.timer is not None:
-            info.object.timer.Stop()
+        if info.object.gasmixer_broker is not None:
+            info.object.gasmixer_broker.exit_flag = True
+            info.object.gasmixer_broker.join()
         return
 
 
@@ -64,6 +109,10 @@ class GasMixerPanel(HasTraits):
     running_label = Str
     state = Int(State.DISCONNECTED)
     connect_timeout = Int(0)
+    gasmixer_broker = Instance(GasMixerSubscriber)
+    subscribe_queue = Instance(Queue.Queue, ())
+    control_queue = Instance(Queue.Queue, ())
+
 
     traits_view = View(Item('control_gasmixer', label='Follow Start/Stop from GasMixer', enabled_when='state is 3'),
                        Item('current_column_int', label='Curr. column'),
@@ -71,9 +120,9 @@ class GasMixerPanel(HasTraits):
                        handler=GasMixerPanelHandler)
 
     def _onTimer(self):
-        socks = dict(self.subscriber_poller.poll(POLL_TIMEOUT))
-        if self.subscriber in socks and socks[self.subscriber] == zmq.POLLIN:
-            msg = self.subscriber.recv()
+        if not self.subscribe_queue.empty():
+            msg = self.subscribe_queue.get()
+            self.subscribe_queue.task_done()
             self.connect_timeout = 0
             if msg.find('NEWCOL') != -1:
                 self.current_column = ({self.x_units[0]:0},
@@ -91,8 +140,15 @@ class GasMixerPanel(HasTraits):
                         self.active_instrument.start_stop = True
                 logger.info('Starting measurement')
             elif msg.find('HEARTBEAT') != -1:
-                self.state = State.CONNECTED
-                self.running_label = 'GasMixer ' + State.strings[self.state]
+                if self.state != State.CONNECTED:
+                    self.control_queue.put('CONNECT')
+            elif msg.find('CONFIG') != -1:
+                    logger.info(json.loads(msg[6:]))
+                    #logger.info(msg[6:])
+                    self.state = State.CONNECTED
+                    self.running_label = 'GasMixer ' + State.strings[self.state]
+            elif msg.find('KILLINGMYSELF'):
+                self.timer.stop()
             return
         self.connect_timeout += UPDATE_INTERVAL
         if self.connect_timeout > CONNECT_TIMEOUT:
@@ -101,15 +157,13 @@ class GasMixerPanel(HasTraits):
 
     def __init__(self, **traits):
         if USE_ZMQ is False:
-            return
+            return None
         super(GasMixerPanel, self).__init__(**traits)
-        self.context = zmq.Context.instance()
-
-        self.subscriber = self.context.socket(zmq.SUB)
-        self.subscriber.connect('tcp://localhost:5561')
-        self.subscriber.setsockopt(zmq.SUBSCRIBE, "")
-        self.subscriber_poller = zmq.Poller()
-        self.subscriber_poller.register(self.subscriber, zmq.POLLIN)
+        self.gasmixer_broker = GasMixerSubscriber(self.subscribe_queue, self.control_queue)
+        if self.gasmixer_broker is None:
+            return None
+        else:
+            self.gasmixer_broker.start()
         self.state = State.DISCONNECTED
         self.timer = Timer(UPDATE_INTERVAL, self._onTimer)
 
@@ -131,7 +185,7 @@ class GasMixerPanel(HasTraits):
 if __name__ == '__main__':
     l = logging.getLogger()
     console = logging.StreamHandler()
-#    l.addHandler(console)
+    l.addHandler(console)
     l.setLevel(logging.DEBUG)
     l.info('test')
     g = GasMixerPanel()

0b832074454b8c0d0304692a98d1b665ae22f0f2
petmo47 <peter.moller@liu.se>
2017-05-04 14:58:59 +0200

    Fixes for SENSiCs CU

--- program/instruments/sourcemeter.py
+++ program/instruments/sourcemeter.py
@@ -262,8 +262,8 @@ class SourceMeter(HasTraits):
         self.sample_number = 0
 
         if self.running:
-            self.timer = Timer.singleShot(max(((self.sample_number+1) * self.sampling_interval -
-                                           (time()-self.start_time))*1000, 0.01),
+            self.timer = Timer.singleShot(max(((self.sample_number + 1) * self.sampling_interval -
+                                               (time() - self.start_time)) * 1000, 0.01),
                                           self._on_timer)
 
     def stop(self):
@@ -306,8 +306,8 @@ class SourceMeter(HasTraits):
             self.dispatch_data(data)
 
         if self.running:
-            self.timer = Timer.singleShot(max(((self.sample_number+1) * self.sampling_interval -
-                                           (time()-self.start_time))*1000, 0.01),
+            self.timer = Timer.singleShot(max(((self.sample_number + 1) * self.sampling_interval -
+                                               (time() - self.start_time)) * 1000, 0.01),
                                           self._on_timer)
 
     def dispatch_data(self, data):
